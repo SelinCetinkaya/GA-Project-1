1546c3f67ec519aabafa3fb82c736e6adab98fa2
Selin Cetinkaya <selin.s.cetinkaya@gmail.com>
2021-02-23 14:41:43 -0600

    random recipe button api call logging to console

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+//==========================================
+//Categories Dropdown
+//==========================================
+
 async function getCategories() {
   const categoriesUrl = 'https://www.themealdb.com/api/json/v1/1/categories.php'
   try {
     const response = await axios.get(categoriesUrl)
     const categoriesArray = response.data.categories
     optionValue(categoriesArray)
-    console.log(categoriesArray)
+    // console.log(categoriesArray)
   } catch (err) {
     console.error(err)
   }
@@ -17,7 +21,30 @@ function optionValue(categoryList) {
     const option = document.createElement('option')
     option.textContent = categoryList[i].strCategory
     getCategories.value = categoryList[i].strCategory
-    console.log(categoryList[i].strCategory)
+    // console.log(categoryList[i].strCategory)
     dropdown.append(option)
   }
 }
+
+//=========================================
+//Random recipe button
+//=========================================
+
+function getValue(e) {
+  e.preventDefault()
+  randomRecipe()
+}
+
+async function randomRecipe() {
+  const randomUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
+  try {
+    const response = await axios.get(randomUrl)
+    console.log(response)
+    return response
+    // appendRecipe()
+  } catch (err) {
+    console.error(err)
+  }
+}
+const randomButton = document.querySelector('#random')
+randomButton.addEventListener('click', getValue)
