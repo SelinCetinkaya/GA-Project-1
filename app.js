e32d61b9325ae6181d8b27fa078544d2010c6c15
Selin Cetinkaya <selin.s.cetinkaya@gmail.com>
2021-02-23 17:31:10 -0600

    all 3 search api calls logging to console

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
+//=========================================
+//Search by name 
+//=========================================
+
+async function getRecipeName(input) {
+  const nameEndpoint = `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`
+  try {
+    const response = await axios.get(nameEndpoint)
+    //append data to dom
+    console.log(response.data.meals)
+    return response
+  } catch (err) {
+    console.error(err)
+  }
+}
+// getRecipeName('spaghetti')
+
+function getNameValue(e) {
+  e.preventDefault
+  const inputValue = document.querySelector('#input').value
+  getRecipeName(inputValue)
+}
+
+const nameForm = document.querySelector('#search')
+nameForm.addEventListener('click', getNameValue)
+
 //==========================================
 //Categories Dropdown
 //==========================================
+
 
 async function getCategories() {
   const categoriesUrl = 'https://www.themealdb.com/api/json/v1/1/categories.php'
@@ -26,12 +53,35 @@ function optionValue(categoryList) {
   }
 }
 
+function getCat(e) {
+  e.preventDefault()
+  //remove data
+  const categoryValue = document.querySelector('#category').value
+  getCategoryItems(categoryValue)
+}
+
+async function getCategoryItems(category) {
+  const categoryItemUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
+  try {
+    const reseponse = await axios.get(categoryItemUrl)
+    //append data here
+    console.log(reseponse.data.meals)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+
+const categoryForm = document.querySelector('.category-search')
+categoryForm.addEventListener('submit', getCat)
+
 //=========================================
 //Random recipe button
 //=========================================
 
-function getValue(e) {
+function getRandom(e) {
   e.preventDefault()
+  //remove data
   randomRecipe()
 }
 
@@ -39,7 +89,7 @@ async function randomRecipe() {
   const randomUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
   try {
     const response = await axios.get(randomUrl)
-    console.log(response)
+    console.log(response.data.meals)
     return response
     // appendRecipe()
   } catch (err) {
@@ -47,4 +97,6 @@ async function randomRecipe() {
   }
 }
 const randomButton = document.querySelector('#random')
-randomButton.addEventListener('click', getValue)
+randomButton.addEventListener('click', getRandom)
+
+
