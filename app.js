6195a3f7e9298f099ce05c61c441d9a6812ae4dd
Selin Cetinkaya <selin.s.cetinkaya@gmail.com>
2021-02-25 15:23:05 -0600

    basic but presentable css

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+//========================================
+//Random food image
+//========================================
 
+async function getRandomImage() {
+  const randomImageUrl = 'https://foodish-api.herokuapp.com/api/'
+  try {
+    const randomImage = await axios.get(randomImageUrl)
+    const appendImage = `<img id="header-image" src=${randomImage.data.image} alt="header-image"/>`
+    document.querySelector('body').insertAdjacentHTML('afterbegin', appendImage)
+    // console.log(randomImage)
+  } catch (err) {
+    console.error(err)
+  }
+}
+getRandomImage()
 
 
 //=========================================
@@ -11,7 +26,8 @@ async function getRecipeName(input) {
     const response = await axios.get(nameEndpoint)
     const mealsArr = response.data.meals
     mealsArr.forEach(meal => {
-      appendRecipe(meal.strMeal, meal.strMealThumb)
+      // appendRecipe(meal.strMeal, meal.strMealThumb)
+      appendRecipe(meal)
     });
     return response
   } catch (err) {
@@ -23,8 +39,10 @@ async function getRecipeName(input) {
 function getNameValue(e) {
   e.preventDefault
   removeRecipes()
-  const inputValue = document.querySelector('#input').value
+  let inputValue = document.querySelector('#input').value
   getRecipeName(inputValue)
+  inputValue = ''
+  console.log(inputValue)
 }
 
 const nameForm = document.querySelector('#search')
@@ -40,6 +58,7 @@ async function getCategories() {
   try {
     const response = await axios.get(categoriesUrl)
     const categoriesArray = response.data.categories
+    // console.log(response)
     optionValue(categoriesArray)
     // console.log(categoriesArray)
   } catch (err) {
@@ -49,7 +68,8 @@ async function getCategories() {
 getCategories()
 
 function optionValue(categoryList) {
-  const dropdown = document.querySelector('#category')
+  const dropdown = document.querySelector('#categories')
+  // console.log(dropdown)
   for (let i = 0; i < categoryList.length; i++) {
     const option = document.createElement('option')
     option.textContent = categoryList[i].strCategory
@@ -62,7 +82,7 @@ function optionValue(categoryList) {
 function getCat(e) {
   e.preventDefault()
   removeRecipes()
-  const categoryValue = document.querySelector('#category').value
+  const categoryValue = document.querySelector('#categories').value
   getCategoryItems(categoryValue)
 }
 
@@ -73,19 +93,15 @@ async function getCategoryItems(category) {
     const response = await axios.get(categoryItemUrl)
     const categoriesArr = response.data.meals
     categoriesArr.forEach(meal => {
-      appendRecipe(meal.strMeal, meal.strMealThumb)
+      appendRecipe(meal)
     });
-    // categoriesArr.forEach(meal => {
-    //   console.log(meal.idMeal)
-    // });
-    //append data here
   } catch (err) {
     console.error(err)
   }
 }
 
 
-const categoryForm = document.querySelector('.category-search')
+const categoryForm = document.querySelector('#category-search')
 categoryForm.addEventListener('submit', getCat)
 
 //=========================================
@@ -103,8 +119,8 @@ async function randomRecipe() {
   try {
     const response = await axios.get(randomUrl)
     const random = response.data.meals
-    console.log(random)
-    appendRecipe(random[0].strMeal, random[0].strMealThumb)
+    // console.log(random)
+    appendRecipe(random[0])
     return response
     // appendRecipe()
   } catch (err) {
@@ -118,14 +134,44 @@ randomButton.addEventListener('click', getRandom)
 //==================================
 //Append
 //==================================
+async function getFullRecipe(id) {
+  // console.log(id)
+  const fullRecipeUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  try {
+    const response = await axios.get(fullRecipeUrl)
+    // console.log(response)
+    return response.data.meals[0].strSource
+  } catch (err) {
+    console.error(err)
+  }
+}
 
-function appendRecipe(title, imgSRC) {
+async function redirectToRecipe(meal) {
+  // console.log(meal.idMeal)
+  const source = meal.strSource || await getFullRecipe(meal.idMeal)
+  const element = document.createElement('a')
+  element.setAttribute('target', '_blank')
+  element.setAttribute('href', source)
+  console.log(element)
+  element.click()
+}
+
+async function appendRecipe(meal) {
+  // console.log(meal)
+  const title = meal.strMeal
+  const imgSRC = meal.strMealThumb
+  const el = document.createElement('div')
+  el.className = "recipe-element"
   let recipe =
-    `<div class='recipe-element'>
-      <h4 class='title'>${title}</h4>
-      <img class='image' src="${imgSRC}" alt="recipe-image"/>
-    </div>`
-  document.querySelector('.search-results').insertAdjacentHTML('beforeend', recipe)
+    `<h4 class='title'>${title}</h4>
+    <img class='image' src="${imgSRC}" alt="recipe-image"/>`
+  el.insertAdjacentHTML('beforeend', recipe)
+  const moreInfoButton = document.createElement('button')
+  moreInfoButton.innerHTML = "View More"
+  moreInfoButton.className = 'view-more'
+  moreInfoButton.addEventListener('click', () => { redirectToRecipe(meal) })
+  el.append(moreInfoButton)
+  document.querySelector('.search-results').append(el)
 }
 
 
